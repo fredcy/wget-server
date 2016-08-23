eab6fdac34cd3f686ae45d37d3943ffd522732bc
Fred Yankowski <fcy@imsa.edu>
2016-08-23 16:40:53 -0500

    Set Content-Type explicitly for apparent CSS files. Take base directory as flag.

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+// This simple HTTP server serves static files as obtained by "wget -r -p -k"
+// for some site. The files created by wget have any query string in their name
+// and so we treat the requested URLs the same way, treating the query string as
+// part of the static file to be found and returned.
+
 import (
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"time"
+	"path/filepath"
 )
+
+var base = flag.String("base", ".", "base directory for files")
 
 func handler(fs http.Dir) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -16,19 +25,30 @@ func handler(fs http.Dir) http.HandlerFunc {
 
 		log.Printf("handle %v", r.URL)
 
+		// net/http is often able to guess the content-type and add it
+		// automatically, but we need to give it some help for some cases.
+		suffix := filepath.Ext(r.URL.Path)
+		if suffix == ".css" {
+			w.Header().Set("Content-Type", "text/css")
+		}
+
 		f, err := fs.Open(path)
 		if err != nil {
 			log.Printf("Cannot open %v: %v", path, err)
 			fmt.Fprintf(w, "Cannot open %v: %v", path, err)
 		} else {
-			http.ServeContent(w, r, "todo", time.Now(), f)
+			if _, err := io.Copy(w, f); err != nil {
+				log.Printf("Cannot copy %v: %v", path, err)
+				fmt.Fprintf(w, "Cannot copy file")
+			}
 			f.Close()
 		}
 	}
 }
 
 func main() {
-	fs := http.Dir("/Users/fcy/Downloads/imsaalumni.org")
+	flag.Parse()
+	fs := http.Dir(*base)
 
 	http.HandleFunc("/", handler(fs))
 	log.Println("Listening...")
